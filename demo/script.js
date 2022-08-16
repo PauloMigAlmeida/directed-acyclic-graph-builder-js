52bae0c0ab837a7f2957a0e5caafc43e88973ac7
Paulo Almeida <paulo.almeidarodenas@niwa.co.nz>
2022-08-16 13:26:23 +1200

    implement text overflow logic

--- demo/script.js
+++ demo/script.js
@@ -5,3 +5,11 @@ let graph = new Graph('#graph');
 // var graph = new GraphCreator(svg, nodes, edges);
 // graph.setIdCt(2);
 // graph.updateGraph();
+
+let vertex = new Vertex(
+    new MouseCoordinate(100, 100),
+    new ShapeSize(200, 100),
+    'Shuffle Data a b c d e f g h i j k l'
+);
+
+graph.appendVertex(vertex);
