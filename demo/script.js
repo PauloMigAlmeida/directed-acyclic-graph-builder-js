12dfc54372735cdef893e08b6e6258f393eaf555
Paulo Almeida <paulo.almeidarodenas@niwa.co.nz>
2022-08-17 14:54:28 +1200

    draw input/output connectors

--- demo/script.js
+++ demo/script.js
@@ -1,6 +1,13 @@
 'use strict';
 
-import {Graph, Vertex, MouseCoordinate, ShapeSize} from "./node_modules/dag-js/lib/dag.js";
+import {
+    Graph,
+    Vertex,
+    MouseCoordinate,
+    ShapeSize,
+    InputVertexConnector,
+    OutputVertexConnector
+} from "./node_modules/dag-js/lib/dag.js";
 
 //TODO: Implement Drag&Drop events
 
@@ -9,15 +16,29 @@ let graph = new Graph('#graph');
 graph.appendVertex(new Vertex(
     new MouseCoordinate(100, 100),
     new ShapeSize(200, 100),
-    'Shuffle Data a b c d e f g h i j k l'
+    'Shuffle Data',
+    [
+        new InputVertexConnector(0, 'data_in', "List[List[List[float]]]"),
+        new InputVertexConnector(1, 'seed_asda_asdas', 'int'),
+    ],
+    [
+        new OutputVertexConnector(0, 'data', 'List[float]'),
+    ],
 ));
 
 graph.update();
 
 graph.appendVertex(new Vertex(
     new MouseCoordinate(300, 300),
-    new ShapeSize(200, 100),
-    'Shuffle Data a b c d e f g h i j k l'
+    new ShapeSize(200, 150),
+    'Shuffle Data a b c d e f g h i j k l',
+    [
+        new InputVertexConnector(0, 'data', "List[float]"),
+        new InputVertexConnector(1, 'seed', 'int'),
+    ],
+    [
+        new OutputVertexConnector(0, 'data', 'List[float]'),
+    ],
 ));
 
 graph.update();
